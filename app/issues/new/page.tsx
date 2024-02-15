20efc390e9531d10c22b3807c2ba15ef7c80a61b
Prasad Patil <patil.prasad76@gmail.com>
2024-02-15 15:09:17 -0800

    Refactor: Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -28,6 +28,18 @@ const NewIssuePage = () => {
 		resolver: zodResolver(createIssueSchema),
 	});
 
+	const onSubmit = handleSubmit(async (data) => {
+		try {
+			setSubmitting(true);
+			await axios.post("/api/issues", data);
+			router.push("/issues");
+		} catch (error) {
+			setError("An unexpected error occured.");
+		} finally {
+			setSubmitting(false);
+		}
+	});
+
 	return (
 		<div className="max-w-xl">
 			{error && (
@@ -35,20 +47,7 @@ const NewIssuePage = () => {
 					<Callout.Text>{error}</Callout.Text>
 				</Callout.Root>
 			)}
-			<form
-				className="space-y-3"
-				onSubmit={handleSubmit(async (data) => {
-					try {
-						setSubmitting(true);
-						await axios.post("/api/issues", data);
-						router.push("/issues");
-					} catch (error) {
-						setError("An unexpected error occured.");
-					} finally {
-						setSubmitting(false);
-					}
-				})}
-			>
+			<form className="space-y-3" onSubmit={onSubmit}>
 				<TextField.Root>
 					<TextField.Input
 						placeholder="Title"
