40cf367534029698abd1bbac60b9a92364185939
Prasad Patil <patil.prasad76@gmail.com>
2024-02-19 15:18:28 -0800

    Set UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Container, Theme } from "@radix-ui/themes";
+import { Container, Theme, ThemePanel } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -29,7 +29,7 @@ export default function RootLayout({
 			<body>
 				<QueryClientProvider>
 					<AuthProvider>
-						<Theme appearance="light" accentColor="purple">
+						<Theme appearance="light" panelBackground="solid">
 							<NavBar />
 							<main className="p-5">
 								<Container>{children}</Container>
